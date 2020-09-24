4fb1e67d8fa24fb8a98764be4550f24aade2d9f4
Слутских Дмитрий Алексеевич <slutskix.2017@stud.nstu.ru>
2020-09-24 11:01:22 +0700

    Изменить проект

--- currentconditionsdisplay.cpp
+++ currentconditionsdisplay.cpp
@@ -10,11 +10,12 @@ void CurrentConditionsDisplay::update(float t, float h, float p){
     this->temperature = t;
     this->humidity = h;
     this->pressure = p;
+    this->display();
 }
 
 void CurrentConditionsDisplay::display(){
     cout << "Current conditions:" << endl << "\t"
          << temperature << "°C" << endl
-         << "%" << humidity << endl
-         << pressure << "mm Hg" << endl;
+         << "\t%" << humidity << endl
+         << "\t" << pressure << "mm Hg" << endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <QCoreApplication>
+#include "weatherdata.h"
+#include "currentconditionsdisplay.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    WeatherData *weatherData = new WeatherData();
+
+    CurrentConditionsDisplay *current = new CurrentConditionsDisplay(weatherData);
+    weatherData->setmeasurementsChanged(80, 65, 30.4);
+    cout << "" << endl;
+    weatherData->setmeasurementsChanged(8, 90, 174);
 
     return a.exec();
 }
